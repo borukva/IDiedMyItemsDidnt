3074e55f63ae721eb9f16e895971f3fcf7ca7c8f
zefir <stickulzon@gmail.com>
2025-09-12 21:17:14 +0200

    Update to 1.21.8

--- src/main/java/net/diemond_player/idmid/mixin/PlayerEntityMixin.java
+++ src/main/java/net/diemond_player/idmid/mixin/PlayerEntityMixin.java
@@ -1,21 +1,20 @@
 package net.diemond_player.idmid.mixin;
 
-import com.llamalad7.mixinextras.sugar.Local;
 import net.diemond_player.idmid.util.IDMIDAccessor;
 import net.minecraft.entity.ItemEntity;
-import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
-@Mixin(PlayerEntity.class)
+@Mixin(LivingEntity.class)
 public abstract class PlayerEntityMixin {
-    @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/ItemEntity;setPickupDelay(I)V"), method = "dropItem(Lnet/minecraft/item/ItemStack;ZZ)Lnet/minecraft/entity/ItemEntity;")
-    private void idmid$dropItem(ItemStack stack, boolean throwRandomly, boolean retainOwnership, CallbackInfoReturnable<ItemEntity> cir, @Local ItemEntity itemEntity) {
+    @ModifyVariable(at = @At(value = "RETURN", ordinal = 2), method = "dropItem")
+    private ItemEntity idmid$dropItem(ItemEntity value, ItemStack stack, boolean dropAtSelf, boolean retainOwnership) {
         if(((IDMIDAccessor)((Object)stack)).idmid$isDeathDrop()){
-            ((IDMIDAccessor)itemEntity).idmid$setItemAge(-32768);
+            ((IDMIDAccessor)value).idmid$setItemAge(-32768);
         }
+        return value;
     }
 }
