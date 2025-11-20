1eddf072ada6b8dc14f37a1ea3951bf4a52a6da7
zefir <stickulzon@gmail.com>
2025-11-20 09:59:34 +0100

    Fix: Armor disappears when player dies

---
+++ src/main/java/net/diemond_player/idmid/mixin/EntityEquipmentMixin.java
@@ -0,0 +1,20 @@
+package net.diemond_player.idmid.mixin;
+
+import net.diemond_player.idmid.util.IDMIDAccessor;
+import net.diemond_player.idmid.util.IDMIDTags;
+import net.minecraft.entity.EntityEquipment;
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+@Mixin(EntityEquipment.class)
+public abstract class EntityEquipmentMixin {
+    @ModifyArg(at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;dropItem(Lnet/minecraft/item/ItemStack;ZZ)Lnet/minecraft/entity/ItemEntity;"), method = "dropAll")
+    private ItemStack idmid$dropAll(ItemStack stack) {
+        if (!(stack.isIn(IDMIDTags.Items.FORCE_DESPAWN))) {
+            ((IDMIDAccessor) ((Object) stack)).idmid$setAsDeathDrop(true);
+        }
+        return stack;
+    }
+}
